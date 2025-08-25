bff5bc62e65884ef4dec564518fd4c0f6ec1dae2
Azkaa Shahnawaz Mulla <azkaasmulla1627@gmail.com>
2025-08-25 14:43:54 +0530

    Added Day 15(15.2) - Checkerboard Pattern Program

---
+++ Day15(15.2)_Checkerboard_Pattern.py
@@ -0,0 +1,12 @@
+# Program 2 - Program to print a checkerboard pattern using stars
+
+rows = 6
+cols = 6
+
+for i in range(rows):
+    for j in range(cols):
+        if (i + j) % 2 == 0:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
