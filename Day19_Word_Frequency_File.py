d5eed42031fdcbb69a4c87797f5e20f5fded2d12
Azkaa Shahnawaz Mulla <azkaasmulla1627@gmail.com>
2025-09-01 22:10:06 +0530

    Added Day 19 - Count Word Frequency in a Text File

--- Day19_Word_Frequency_File.py
+++ Day19_Word_Frequency_File.py
@@ -1,7 +1,7 @@
 # Day 19
 # Program : To count word frequency in a text file
 
-def word_frequency(sample):
+def word_frequency(filename):
     freq = {}
     try:
         with open(filename, "r") as file:
